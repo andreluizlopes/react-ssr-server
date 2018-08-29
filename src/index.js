807270360e17371337405fbdad60fac7bf73ceda
André <lopes.andree@gmail.com>
2018-08-29 00:41:37 -0300

    error handling

--- src/index.js
+++ src/index.js
@@ -22,16 +22,29 @@ app.get('*', (req, res) => {
 
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
-  });
+  })
+  .map(promise => {
+    if (promise) {
+      return new Promise((resolve, reject) => {
+        promise.then(resolve).catch(resolve);
+      })
+    }
+  })
 
   Promise.all(promises).then(() => {
     const context = {};
     const content = renderer(req, store, context);
+
+    if (context.url) {
+      return res.redirect(301, constext.url);
+    }
     if (context.notFound) {
       res.status(404);
     }
 
     res.send(content);
+  }).catch(() => {
+    res.send('Somethings went wrong');
   });
 
 
